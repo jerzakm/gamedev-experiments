f18d632ecfd8ec41fc17095cc260760f9b736894
jerzakm <jerzakm@gmail.com>
2021-10-28 13:59:39 +0200

    - vite TS vanilla template - pixi install

---
+++ matter-ecs-buffer/src/main.ts
@@ -0,0 +1,9 @@
+import "./style/global.css";
+import "./renderer";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
