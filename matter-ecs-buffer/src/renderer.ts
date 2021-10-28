2758f15589eb71bec13bb09297dc79ba1b8145a8
jerzakm <jerzakm@gmail.com>
2021-10-28 14:09:53 +0200

    - renderer Class - main setup

--- matter-ecs-buffer/src/renderer.ts
+++ matter-ecs-buffer/src/renderer.ts
@@ -1,40 +1,29 @@
 import * as PIXI from "pixi.js";
 
-const app = new PIXI.Application({
-  width: 800,
-  height: 600,
-  backgroundColor: 0x1099bb,
-  resolution: window.devicePixelRatio || 1,
-});
-document.body.appendChild(app.view);
+export class Renderer {
+  app: PIXI.Application;
+  stage: PIXI.Container;
 
-const container = new PIXI.Container();
+  constructor() {
+    this.app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      backgroundColor: 0xcecece,
+      resolution: window.devicePixelRatio || 1,
+    });
+    document.body.appendChild(this.app.view);
+    this.app.view.id = "pixi-view";
 
-app.stage.addChild(container);
+    this.stage = this.app.stage;
 
-// Create a new texture
-const texture = PIXI.Texture.from("square.png");
+    this.resize();
+  }
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-  const bunny = new PIXI.Sprite(texture);
-  bunny.anchor.set(0.5);
-  bunny.x = (i % 5) * 40;
-  bunny.y = Math.floor(i / 5) * 40;
-  container.addChild(bunny);
+  private resize() {
+    // resize canvas and webgl renderer when window sizeChanges
+    window.addEventListener("resize", () => {
+      this.app.view.width = window.innerWidth;
+      this.app.view.height = window.innerHeight;
+    });
+  }
 }
-
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
-
-// Listen for animate update
-app.ticker.add((delta) => {
-  // rotate the container!
-  // use delta to create frame-independent transform
-  container.rotation -= 0.01 * delta;
-});
