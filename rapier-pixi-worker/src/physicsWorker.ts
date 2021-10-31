e4264c64d0fe30a53c7579af6e6dd598c886e14c
Marcin Jerzak <jerzakm@gmail.com>
2021-10-31 20:01:12 +0100

    copied previous example, removed physics engine

---
+++ rapier-pixi-worker/src/physicsWorker.ts
@@ -0,0 +1,55 @@
+const maxFps = 60;
+const deltaGoal = 1000 / maxFps;
+
+const runner = (delta = 16) => {
+  const startTs = performance.now();
+
+  // Engine.update(physics.engine, delta);
+
+  if (Math.random() > 0.3) {
+    // physics.applyForceToRandomBody();
+  }
+
+  self.postMessage({
+    type: "BODY_SYNC",
+    // data: physics.getBodySyncData(),
+    delta,
+  });
+
+  const currentDelta = performance.now() - startTs;
+
+  // this bit limits max FPS to 60
+  const deltaGoalDifference = Math.max(0, deltaGoal - currentDelta);
+  const d = Math.max(currentDelta, deltaGoal);
+
+  setTimeout(() => runner(d), deltaGoalDifference);
+};
+
+runner();
+
+// once a second check for bodies out of bound
+setInterval(() => {
+  // physics.outOfBoundCheck();
+}, 1000);
+
+self.addEventListener("message", (e) => {
+  const message = e.data || e;
+
+  if (message.type == "ADD_BODY") {
+    const { x, y, width, height, options } = message.data;
+    // const body = physics.addBody(x, y, width, height, options);
+
+    self.postMessage({
+      type: "BODY_CREATED",
+      data: {
+        // id: body.id,
+        x,
+        y,
+        width,
+        height,
+        angle: 0,
+        sprite: undefined,
+      },
+    });
+  }
+});
---
+++ rapier-pixi-worker/src/renderer.ts
@@ -0,0 +1,32 @@
+import * as PIXI from "pixi.js";
+
+export class Renderer {
+  app: PIXI.Application;
+  stage: PIXI.Container;
+
+  constructor() {
+    this.app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      backgroundColor: 0xcecece,
+      resolution: 1,
+      antialias: true,
+    });
+    // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+    document.body.appendChild(this.app.view);
+    this.app.view.id = "pixi-view";
+
+    this.stage = this.app.stage;
+
+    this.resize();
+  }
+
+  private resize() {
+    // resize canvas and webgl renderer when window sizeChanges
+    window.addEventListener("resize", () => {
+      this.app.view.width = window.innerWidth;
+      this.app.view.height = window.innerHeight;
+    });
+  }
+}
