98eb6a9a4c116438f9baf1aa11f951970994bab8
Marcin Jerzak <jerzakm@gmail.com>
2021-11-11 19:45:03 +0100

    Refactoring and cleanup - main - renamed function, added comments - removed physics worker - removed uuid4 util

--- rapier-pixi-character-controller-dynamic/src/main.ts
+++ rapier-pixi-character-controller-dynamic/src/main.ts
@@ -9,88 +9,66 @@ import { initEnvBallGraphics } from "./draw/envBallGraphics";
 import { RigidBody, Collider } from "@dimforge/rapier2d-compat";
 import { setupPlayer } from "./player";
 
-async function mainShooter() {
-  // RENDERER
+const BALL_COUNT = 100;
+
+async function start() {
+  // setup the renderer
   const { app, stage } = new Renderer();
   const container = new PIXI.Container();
   stage.addChild(container);
+
+  // individual graphics for walls and balls
   const { wallGraphics, drawWalls } = initWallGraphics();
   const { envBallGraphics, drawEnvBalls } = initEnvBallGraphics();
-
   container.addChild(wallGraphics);
   container.addChild(envBallGraphics);
 
-  // PHYSICS
+  // physics setup
   const physics = await initPhysics({ x: 0, y: 0 });
   const { RAPIER, step, world } = physics;
 
-  // make walls
+  // add walls to the physics world
   const walls = wallScreenArea(world, RAPIER, 50);
 
-  //make balls
+  // add balls to the physics world
   const envBalls: {
     body: RigidBody;
     collider: Collider;
     definition: BallDefinition;
   }[] = [];
 
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < BALL_COUNT; i++) {
     envBalls.push(spawnRandomBall(world, RAPIER));
   }
 
-  //setup player
+  // setup player - physics, graphics & update function
   const { playerGraphics, drawPlayer, updatePlayer } = setupPlayer(
     world,
     RAPIER
   );
-
   container.addChild(playerGraphics);
 
   app.ticker.add((delta) => {
     const d = delta * 0.1;
 
-    // Ball spawns over time
-    // if (Math.random() > 0.99) {
-    //   const bouncyBall = spawnRandomBall(world, RAPIER);
-    //   bouncyBall.collider.setRestitution(1);
-    //   envBalls.push(bouncyBall);
-    // }
-    updatePlayer();
+    // Uncomment to spawn more balls over time
+    /*
+    if (Math.random() > 0.99) {
+      const bouncyBall = spawnRandomBall(world, RAPIER);
+      bouncyBall.collider.setRestitution(1);
+      envBalls.push(bouncyBall);
+    }
+    */
+    updatePlayer(); // Player movement logic from player.ts happens here
     drawWalls(walls);
     drawEnvBalls(envBalls);
-    drawPlayer();
-    step(d);
-
-    app.render();
+    drawPlayer(delta);
+    step(d); // step physics
+    app.render(); // pixi render
   });
 }
 
-mainShooter();
-
-export interface GameObject {
-  id: string | number;
-  type: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  angle: number;
-  shape: string;
-  sprite?: PIXI.Sprite | undefined;
-  graphics?: {
-    draw: boolean;
-    line: number;
-    fill?: number;
-  };
-}
-
-export type PositionSyncMap = {
-  [key: number]: {
-    x: number;
-    y: number;
-    rotation: number;
-  };
-};
+start();
 
 export interface PhysicsObjectOptions {
   isStatic: boolean;
