8bba7f18e327a9f8bc58bc91744be0e156f85386
Marcin Jerzak <jerzakm@gmail.com>
2021-11-12 02:48:44 +0100

    - article v1 - player refactor

---
+++ rapier-pixi-character-controller-dynamic/README.md
@@ -0,0 +1,146 @@
+Following up on my recent 'discovery' of the [Rapier.rs physics engine](https://rapier.rs) I make a first attempt at a character controller.
+
+## Rigid-body choices for a character controller in Rapier.rs
+
+With the exception of `Static` all other body types seem viable to make a controller, namely:
+
+- `KinematicPositionBased`
+- `KinematicVelocityBased`
+- `Dynamic`
+
+Kinematic bodies allow us to set their Position and Velocity, so at a first glance it sounds like they'd make a good controller. Unfortunately they come with a few caveats, making them harder to use than you'd think. The biggest drawback for a quick and easy character controller is the fact that they don't interact with static bodies out of the gate and will clip through them. Not great if we want our character stick to walls and platforms. Rapier provides us with a lot of options to handle this drawback. Scene queries and hooks are quite robust, allowing user to roll their own collision logic, but it's not something I want to get into before learning a bit more about the engine.
+
+The last remaining choice, `Dynamic` is a fully fledged body that interacts with the entire world.
+
+## Setup
+
+To not make this article unnecessarily long, I will skip the world and renderer setup and instead link the github repo for the project. It should be easy enough to follow and you're always welcome to hit me up with any questions you might have.
+
+Before proceeding with character controller I setup:
+
+- rapier.rs physics world with gravity `{x: 0, y: 0}` - for the topdown experience
+- add walls to browser window bounds
+- spawn Dynamic objects for our character to interact with later, in this case 100 randomly sized balls
+- render walls and balls with simple pixi.js graphics
+
+## Step by step
+
+Steps to implement a simple keyboard and point to click controller:
+
+### Player body setup
+
+1. Create a player physics body and place it in the middle of the screen with `setTranslation`
+
+```ts
+const body = world.createRigidBody(
+  RAPIER.RigidBodyDesc.newDynamic().setTranslation(
+    window.innerWidth / 2,
+    window.innerHeight / 2
+  )
+);
+```
+
+2. Make a collider description so the body has shape and size. It needs it to interact with the world. For this example we're going with a simple circle. Translation in this step describes the collider's relative position to the body.
+
+```ts
+const colliderDesc = new RAPIER.ColliderDesc(
+  new RAPIER.Ball(12)
+).setTranslation(0, 0);
+```
+
+3. Create a collider, attach it to the body and add the whole thing to the world.
+
+```ts
+const collider = world.createCollider(colliderDesc, body.handle);
+```
+
+### Keyboard WASD control bindings
+
+In later steps we will move the player's body based on the provided direction. To get that we're going to setup a basic WASD control scheme with listeners listening to `keydown` and `keyup`. They will manipulate a direction vector:
+
+```ts
+const direction = {
+  x: 0,
+  y: 0,
+};
+```
+
+When the key is pressed down, player begins to move:
+
+```ts
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w": {
+      direction.y = -1;
+      break;
+    }
+    case "s": {
+      direction.y = 1;
+      break;
+    }
+    case "a": {
+      direction.x = -1;
+      break;
+    }
+    case "d": {
+      direction.x = 1;
+      break;
+    }
+  }
+});
+```
+
+Then, when the key is released the movement on that particular axis (x or y) is set to 0.
+
+```ts
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w": {
+      direction.y = 0;
+      break;
+    }
+    case "s": {
+      direction.y = 0;
+      break;
+    }
+    case "a": {
+      direction.x = 0;
+      break;
+    }
+    case "d": {
+      direction.x = 0;
+      break;
+    }
+  }
+});
+```
+
+### Moving the body
+
+Now that we've made a way for us to imput where the player has to go, it's time to make it happen. We will create an `updatePlayer` function that will have to be called every frame.
+
+The most basic approach is as simple as the snippet below, we simply set the body's velocity to the `direction`.
+
+```ts
+const updatePlayer = () => {
+  body.setLinvel(direction, true);
+};
+```
+
+You might notice though, that the body isn't moving much. That's because we only set the direction vector to go from -1 to 1, and that isn't very fast. To combat that and make the code more reusable we add a `MOVE_SPEED` variable and multiply the x and y of the direction.
+
+```ts
+const MOVE_SPEED = 50;
+
+const updatePlayer = () => {
+  body.setLinvel(
+    { x: direction.x * MOVE_SPEED, y: direction.y * MOVE_SPEED },
+    true
+  );
+};
+```
+
+That's more like it!
+
+**Bonus method: Applying force to move the body**
+When I was playing around and writing this article I found another cool way to make our player's body move. Instead of setting the velocity directly, we "push" the body to make it go in the desired direction at a desired speed. It gives a smoother, more natural feeling movement right out of the gate.
--- rapier-pixi-character-controller-dynamic/src/player.ts
+++ rapier-pixi-character-controller-dynamic/src/player.ts
@@ -1,29 +1,51 @@
-import { World } from "@dimforge/rapier2d-compat";
+import { World, Vector2 } from "@dimforge/rapier2d-compat";
 import { Graphics } from "pixi.js";
 import { PLAYER } from "./draw/_colorTheme";
 import { RAPIER } from "./physics/core";
 
-const MOVE_SPEED = 100;
-const ACCELERATION = 75;
+const MOVE_SPEED = 80;
+const ACCELERATION = 80;
 
 export const setupPlayer = (world: World, RAPIER: RAPIER) => {
-  const { body, collider } = makePlayerPhysicsBody(world, RAPIER);
+  const updatePlayer = () => {
+    // Mouse only logic start
+    if (goal) {
+      const position = body.translation();
 
-  const playerGraphics = new Graphics();
-
-  let x = 0;
-  let y = 0;
-
-  collider.setActiveHooks(RAPIER.ActiveHooks.FILTER_CONTACT_PAIRS);
-
-  const applyVelocity = () => {
+      const distanceFromGoal = Math.sqrt(
+        (position.x - goal.x) ** 2 + (position.y - goal.y) ** 2
+      );
+      if (distanceFromGoal < 3) {
+        body.setTranslation(goal, true);
+        direction.x = 0;
+        direction.y = 0;
+        goal = undefined;
+      } else {
+        const x = goal.x - position.x;
+        const y = goal.y - position.y;
+        const div = Math.max(Math.abs(x), Math.abs(y));
+        direction.x = x / div;
+        direction.y = y / div;
+      }
+    }
+    // mouseonly logic end
     const velocity = body.linvel();
-    const accelerationX = (x * MOVE_SPEED - velocity.x) * ACCELERATION;
-    const accelerationY = (y * MOVE_SPEED - velocity.y) * ACCELERATION;
-    body.applyImpulse({ x: accelerationX, y: accelerationY }, true);
+
+    // Approach 1 - just setting velocity
+    body.setLinvel(
+      { x: direction.x * MOVE_SPEED, y: direction.y * MOVE_SPEED },
+      true
+    );
+
+    // Approach 2 - Applying impulse to body
+    // const impulse = {
+    //   x: (direction.x * MOVE_SPEED - velocity.x) * ACCELERATION,
+    //   y: (direction.y * MOVE_SPEED - velocity.y) * ACCELERATION,
+    // };
+    // body.applyImpulse(impulse, true);
   };
 
-  const drawPlayer = () => {
+  const drawPlayer = (delta = 16) => {
     playerGraphics.clear();
     playerGraphics.beginFill(PLAYER.fill, PLAYER.alpha);
     playerGraphics.lineStyle({
@@ -35,67 +57,109 @@ export const setupPlayer = (world: World, RAPIER: RAPIER) => {
     const radius = collider.radius();
     playerGraphics.drawCircle(x, y, radius);
     playerGraphics.endFill();
+
+    if (goal) {
+      playerGraphics.lineStyle({
+        alpha: PLAYER.alpha,
+        color: 0xff2222,
+        width: PLAYER.strokeWidth,
+      });
+      playerGraphics.drawCircle(
+        goal.x,
+        goal.y,
+        Math.sin(performance.now()) * 2 * delta + 20
+      );
+    }
   };
 
-  const updatePlayer = () => {
-    applyVelocity();
+  const setupKeyMouseListeners = () => {
+    // move on keydown
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "w": {
+          direction.y = -1;
+          break;
+        }
+        case "s": {
+          direction.y = 1;
+          break;
+        }
+        case "a": {
+          direction.x = -1;
+          break;
+        }
+        case "d": {
+          direction.x = 1;
+          break;
+        }
+      }
+    });
+
+    // stop on keyup
+    window.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "w": {
+          direction.y = 0;
+          break;
+        }
+        case "s": {
+          direction.y = 0;
+          break;
+        }
+        case "a": {
+          direction.x = 0;
+          break;
+        }
+        case "d": {
+          direction.x = 0;
+          break;
+        }
+      }
+    });
+
+    // click to move - set goal
+    document.addEventListener("pointerdown", (e) => {
+      const { clientX, clientY } = e;
+
+      goal = {
+        x: clientX,
+        y: clientY,
+      };
+    });
   };
 
-  window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "w": {
-        y = -1;
-        break;
-      }
-      case "s": {
-        y = 1;
-        break;
-      }
-      case "a": {
-        x = -1;
-        break;
-      }
-      case "d": {
-        x = 1;
-        break;
-      }
-    }
-  });
+  const { body, collider } = makePlayerPhysicsBody(world, RAPIER);
 
-  window.addEventListener("keyup", (e) => {
-    switch (e.key) {
-      case "w": {
-        y = 0;
-        break;
-      }
-      case "s": {
-        y = 0;
-        break;
-      }
-      case "a": {
-        x = 0;
-        break;
-      }
-      case "d": {
-        x = 0;
-        break;
-      }
-    }
-  });
+  const playerGraphics = new Graphics();
+
+  const direction: Vector2 = {
+    x: 0,
+    y: 0,
+  };
+
+  let goal: Vector2 | undefined;
+
+  collider.setActiveHooks(RAPIER.ActiveHooks.FILTER_CONTACT_PAIRS);
+
+  setupKeyMouseListeners();
 
   return { playerGraphics, drawPlayer, updatePlayer };
 };
 
 const makePlayerPhysicsBody = (world: World, RAPIER: RAPIER) => {
+  // create player's body and place it in the middle of the screen
   const body = world.createRigidBody(
-    RAPIER.RigidBodyDesc.newDynamic()
-      .setTranslation(window.innerWidth / 2, window.innerHeight / 2)
-      .setCanSleep(false)
+    RAPIER.RigidBodyDesc.newDynamic().setTranslation(
+      window.innerWidth / 2,
+      window.innerHeight / 2
+    )
   );
+  // create a collider that describes the shape of player's body
   let colliderDesc = new RAPIER.ColliderDesc(
     new RAPIER.Ball(12)
   ).setTranslation(0, 0);
 
+  // attach the collider to the body and add it to the world
   const collider = world.createCollider(colliderDesc, body.handle);
 
   return { body, collider };
